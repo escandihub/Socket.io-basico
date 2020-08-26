242d2fad05833f82457852cd2fd164701e301704
Escandon <aleskndon@hotmail.com>
2020-08-26 10:48:54 -0500

    Mensajes con socketio basico

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'asdsdfas#'
+socketio = SocketIO(app)
+
+
+@app.route('/')
+def sessions():
+    return render_template('session.html')
+
+
+def messageReceived(methods=['GET', 'POST']):
+    print('Mensaje recivido!')
+
+
+@socketio.on('my event')
+def handle_my_custom_event(json, methods=['GET', 'POST']):
+    print('se recivio el my event: ' + str(json))
+    socketio.emit('my response', json, callback=messageReceived)
+
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
